93b9133aae3ce854ae72f48a4a8ec2bc81b720f9
sadeepadilharap <sadeepadilharap@gmail.com>
2022-10-30 22:34:32 +0530

    Added the initial code

---
+++ rectangle-area.py
@@ -0,0 +1,10 @@
+width=input("width of the rectangle = ")
+length=input("length of the rectangle = ")
+width=float(width)
+length=float(length)
+
+
+area=width*length
+print("width =",width)
+print("length =",length)
+print("Area of the rectangle =", area)
